2a545777ec57bf068c08995a3821229584d2cca7
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-05-05 11:17:14 +0100

    Django Model @property

--- Hardik_Patel_Course/ems/ems/settings.py
+++ Hardik_Patel_Course/ems/ems/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-s9dxu93@8es#u!dm7id6kf4mitnxlg=zqkuafgk4jsf3t)-6=4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
