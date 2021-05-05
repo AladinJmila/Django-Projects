c39fbd891f8fce761efe94a00b906f5d546b69ba
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-05-05 17:19:06 +0100

    EMS -> Working with serializers in CLI

--- Hardik_Patel_Course/ems/poll/models.py
+++ Hardik_Patel_Course/ems/poll/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
-  title = models.TextField(null=True, blank=True)
+  title = models.TextField(null=False, blank=False)
   status = models.CharField(default='inactive', max_length=10)
-  created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
   start_date = models.DateTimeField(null=True, blank=True)
   end_date = models.DateTimeField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
