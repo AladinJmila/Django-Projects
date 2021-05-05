3ce7f90e1125f112b766e83ff47376aa915df1a4
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-05-05 15:11:18 +0100

    EMS -> Adding data to db without django forms

--- Hardik_Patel_Course/ems/poll/views.py
+++ Hardik_Patel_Course/ems/poll/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from poll.models import *
 
 def index(request):
@@ -21,3 +21,25 @@ def details(request, id=None):
     'question': question,
   }
   return render(request, 'polls/details.html', context)
+
+def poll(request, id=None):
+  if request.method == 'GET':
+    try:
+      question = Question.objects.get(id=id)
+    except: 
+      raise Http404
+    context = {
+      'question': question
+    }
+    return render(request, 'polls/poll.html', context)
+
+  if request.method == 'POST':
+    user_id = 1
+    print(request.POST)
+    data = request.POST # or can be request.data
+    answer = Answer.objects.create(user_id=user_id, choice_id=data['choice'])
+    if answer:
+      return HttpResponse('Your vote was done successfully.')
+    else: 
+      return HttpResponse('You vote was not done successfully.')
+    
