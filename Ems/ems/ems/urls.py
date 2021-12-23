95a81a3ec618f8f8c49f53a2db7de9a284661592
AladinJmila <jmila.aladin@gmail.com>
2021-12-23 16:59:43 +0000

    Merge attempt

---
+++ Ems/ems/ems/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('employee/', include('employee.urls')),
+    path('polls/', include('poll.urls')),
+    path('api/v1/', include('poll.api_urls')),
+    # path('api/v1/', include('employee.api_urls')),
+]
---
+++ Ems/ems/poll/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from poll.models import Question, Choice
+
+class QuestionSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Question
+    fields = [
+      'id',
+      'title',
+      'status',
+      'created_by'
+    ]
+
+class ChoiceSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Choice
+    fields = '__all__'
